3e22b53577f52a3e36cdbab27dcfd6d13e163d9d
alessandraluiza <aleluizasg@gmail.com>
2024-09-06 17:18:42 -0300

    Exercício 5

--- pratica.py
+++ pratica.py
@@ -1,15 +1,11 @@
-class Restaurante:
-
-    def __init__(self, nome, categoria, ativo=False, especialidade=any, cidade=any):
+class Cliente:
+    def __init__(self, nome, sobrenome, telefone, endereco):
         self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
-        self.especialidade = especialidade
-        self.cidade = cidade
-    
-    def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        self.sobrenome = sobrenome
+        self.telefone = telefone
+        self.endereco = endereco
 
-# Exibindo uma instância do restaurante formatada
-novo_restaurante = Restaurante(nome='Camilos', categoria='Brasileira')
-print(novo_restaurante)
+# Instanciando três objetos da classe Cliente e atribuindo valores aos seus atributos através do construtor
+cadastro1 = Cliente(nome='Alice', sobrenome='Souza', telefone='62-96325654')
+cadastro2 = Cliente(nome='Raul', sobrenome='Greg', telefone='83-84569711')
+cadastro3 = Cliente(nome='Sofia', sobrenome='Matos', telefone='42-85541236')
