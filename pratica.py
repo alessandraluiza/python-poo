66b10f5eec3349c43d8880c87402e963d8d15127
alessandraluiza <aleluizasg@gmail.com>
2024-09-06 16:22:49 -0300

    Exercício 1

--- pratica.py
+++ pratica.py
@@ -1,24 +1,9 @@
-class Musica:
-    musicas = []
+class Carro:
 
-    def __init__(self, nome='', artista='', duracao=0):
-        self.nome = nome
-        self.artista = artista
-        self.duracao = duracao
-        Musica.musicas.append(self)
+    def __init__(self, modelo, cor, ano):
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
 
-    def __str__(self):
-        return f'{self.nome} | {self.artista}'
-    
-    def listar_musicas():
-        for musica in Musica.musicas:
-            print(f'{musica.nome} | {musica.artista} | {musica.duracao}')
-
-
-musica1 = Musica(nome='Beija-flor', artista='Victor e Vinicius', duracao=245)
-
-musica2 = Musica(nome='Estilado', artista='Marcinho', duracao=326)
-
-musica3 = Musica(nome='Mulher ingrata', artista='Roberto', duracao=298)
-
-Musica.listar_musicas()
+# Instanciando um carro e atribuindo valores aos seus atributos
+meu_carro = Carro(modelo = 'Virtus', cor='Branco neve', ano=2019)
