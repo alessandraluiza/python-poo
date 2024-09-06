73959269847402429a034ecaf03f7c2d690fb9e4
alessandraluiza <aleluizasg@gmail.com>
2024-09-06 16:33:03 -0300

    Exercício 2

--- pratica.py
+++ pratica.py
@@ -1,9 +1,11 @@
-class Carro:
+class Restaurante:
 
-    def __init__(self, modelo, cor, ano):
-        self.modelo = modelo
-        self.cor = cor
-        self.ano = ano
+    def __init__(self, nome, categoria, especialidade, cidade):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        self.especialidade = especialidade
+        self.cidade = cidade
 
-# Instanciando um carro e atribuindo valores aos seus atributos
-meu_carro = Carro(modelo = 'Virtus', cor='Branco neve', ano=2019)
+# Instanciando um restaurante e atribuindo valores aos seus atributos
+restaurante_praga = Restaurante(nome='Praga', categoria='Italiana', ativo=False, especialidade='Fettuccine', cidade='Asa Sul')
