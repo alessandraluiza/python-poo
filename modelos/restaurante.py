2fe7d0867605f6ce81ca5d881e4179d5ec065778
alessandraluiza <aleluizasg@gmail.com>
2024-09-06 15:50:32 -0300

    Atualização

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,27 +1,20 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Bistrô'
-restaurante_praca.categoria = 'Italiana'
+    def __init__(self, nome, categoria ):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-nome_do_restaurante = restaurante_praca.nome
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes(): 
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-if restaurante_praca.ativo:
-    print('Restaurante ativo')
-else:
-    print('Restaurante desativado')
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-categoria = Restaurante.categoria
-
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = 'Pizza Place'
-restaurante_pizza.categoria = 'Fast Food'
-
-print(restaurante_pizza.categoria)
-
-restaurante_pizza.ativo = True
-
-print(f'Nome: {restaurante_pizza.nome}, Categoria: {restaurante_pizza.categoria}')
+Restaurante.listar_restaurantes()
