a037380f515a94004fa8dacf09942f8bd169c339
alessandraluiza <aleluizasg@gmail.com>
2024-09-06 17:04:14 -0300

    Exercício 4

--- pratica.py
+++ pratica.py
@@ -1,11 +1,15 @@
 class Restaurante:
 
-    def __init__(self, nome, categoria, especialidade, cidade):
+    def __init__(self, nome, categoria, ativo=False, especialidade=any, cidade=any):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
         self.especialidade = especialidade
         self.cidade = cidade
+    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
 
-# Instanciando um restaurante utilizando o construtor
+# Exibindo uma instância do restaurante formatada
 novo_restaurante = Restaurante(nome='Camilos', categoria='Brasileira')
+print(novo_restaurante)
