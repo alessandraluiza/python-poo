192e90e7ad039f38e457830f5f2f4f29cd9f4d5f
alessandraluiza <aleluizasg@gmail.com>
2024-09-09 13:50:18 -0300

    Refatorando uma função

--- pratica.py
+++ pratica.py
@@ -1,11 +1,39 @@
-class Cliente:
-    def __init__(self, nome, sobrenome, telefone, endereco):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.telefone = telefone
-        self.endereco = endereco
+class Pessoa:
+    
+    def __init__ (self, nome='', idade=0, profissao=''):
+        self.nome = nome.title()
+        self.idade = idade
+        self.profissao = profissao.title()
 
-# Instanciando três objetos da classe Cliente e atribuindo valores aos seus atributos através do construtor
-cadastro1 = Cliente(nome='Alice', sobrenome='Souza', telefone='62-96325654')
-cadastro2 = Cliente(nome='Raul', sobrenome='Greg', telefone='83-84569711')
-cadastro3 = Cliente(nome='Sofia', sobrenome='Matos', telefone='42-85541236')
+    def __str__(self):
+        return f'Nome: {self.nome}\nIdade: {self.idade} \nProfissão: {self.profissao}'
+    
+    def aniversario(self):
+        self.idade += 1
+    
+    @property
+    def saudacao(self):
+        return f'Olá, {self.nome}! Como está o trabalho como {self.profissao}?'
+
+pessoa1 = Pessoa("Maria", 30, "engenheira")
+pessoa2 = Pessoa("João", 27, "programador")
+pessoa3 = Pessoa("Benjamin", 55, "advogado")
+
+print('Informações Funcionários:')
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
+print()
+
+
+pessoa1.aniversario()
+pessoa2.aniversario()
+
+print('Informações seguinte aniversário:')
+print(pessoa1)
+print(pessoa2)
+print()
+
+
+print(pessoa3.saudacao)
+
