db3c67668dc9d98a74368675391bf4821acf20ea
alessandraluiza <aleluizasg@gmail.com>
2024-09-13 14:40:08 -0300

    Implementando Avaliações

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
-restaurante_japones = Restaurante('Japa', 'Japonesa')
-
-restaurante_mexicano.alternar_estado()
+restaurante_praca.receber_avaliacao('Alessandra', 8)
+restaurante_praca.receber_avaliacao('Marcilio', 5)
+restaurante_praca.receber_avaliacao('Ruana', 6)
 
 def main():
     Restaurante.listar_restaurantes()
