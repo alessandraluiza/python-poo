8f72505f148b0a97412a074537d7beaf7cfc7972
alessandraluiza <aleluizasg@gmail.com>
2024-09-12 23:32:03 -0300

    From e Import

---
+++ app.py
@@ -0,0 +1,11 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
