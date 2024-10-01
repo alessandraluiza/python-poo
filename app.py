51203a7c9878ae66a9ac7881fb36ad30451b5852
alessandraluiza <aleluizasg@gmail.com>
2024-10-01 14:35:35 -0300

    Prática de herança e modularização

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Alessandra', 8)
-restaurante_praca.receber_avaliacao('Marcilio', 5)
-restaurante_praca.receber_avaliacao('Ruana', 6)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+
+
+
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
